e1b5d42de0911bcf61a5cbe327c7ae5e49b79da9
Coolbro2012 <87591452+Coolbro2012@users.noreply.github.com>
2023-12-20 12:32:23 -0500

    Create lilfooter.js

---
+++ lilfooter.js
@@ -0,0 +1,16 @@
+var footer = `
+        <footer style="background-color: var(--lighter-color); width: 100%; float: left; padding: 0 0 0 0;">
+            <div class="randombox">
+                <ul style=" gap: 5rem; list-style: none; position: relative; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+                    <li id="randommessage" style="color: var(--offset-color);">Test</li>
+                </ul>
+            </div>
+            <hr>
+            <ul style="display: flex; gap: 5rem; list-style: none;">
+                <li style="color: var(--offset-color);">Creator/Founder: Grin</li>
+                <li style="color: var(--offset-color);">Email Us: Xenapsis@gmail.com</li>
+                <li style="color: var(--offset-color);">Version 2.8.8</li>
+            </ul>
+        </footer>`;
+
+        document.querySelector("#footerthing").insertAdjacentHTML("beforeend", footer)
